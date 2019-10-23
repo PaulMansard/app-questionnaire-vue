c9b8f24633c071f12439f34f42a0b8dbc8c29959
PaulMansard <mansard.paul@gmail.com>
2019-10-23 12:01:58 +0200

    test

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import Vue from 'vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import BootstrapVue from 'bootstrap-vue'
 
-Vue.use(BootstrapVue)
-
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
 
 new Vue({
   router,
--- src/views/Connexion.vue
+++ src/views/Connexion.vue
@@ -1,7 +1,22 @@
 <template>
   <div class="connexion">
     <Connexion msg="Welcome to Your Vue.js Appaaqaqqaezz"/>
+    <b-form @submit="onSubmit">
+      <b-form-group label-cols-sm="2" label-cols-lg="2" label-for="input-horizontal" id="groupNom" label="Nom : ">
+        <b-form-input class="w-75" id="input-horizontal" placeholder="Entrez votre nom" type="text" v-model="nom"></b-form-input>
+      </b-form-group>
+      <b-form-group id="groupPrenom" label="Prénom : " label-cols-sm="2" label-cols-lg="2" label-for="input-2">
+        <b-form-input class="w-75" id="inputPrenom" placeholder="Entez votre prénom" v-model="prenom"></b-form-input>
+      </b-form-group>
+      <b-form-group id="inputEntreprise" label="Entreprise : " label-cols-sm="2" label-cols-lg="2" label-for="input-2">
+        <b-form-input class="w-75" id="inputEntrerise" placeholder="Entez votre entreprise" v-model="entreprise"></b-form-input>
+      </b-form-group>
+    </b-form>
+    <b-button type="submit" variant="primary">Connexion</b-button>
   </div>
+  <b-card class="mt-3" header="Form Data Result">
+    <pre class="m-0">{{ form }}</pre>
+  </b-card>
 </template>
 
 <script>
@@ -12,6 +27,21 @@ export default {
   name: 'connexion',
   components: {
     Connexion
+  },
+  data () {
+    return {
+      form: {
+        nom: '',
+        prenom: '',
+        entreprise: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit (evt) {
+      evt.preventDefault()
+      alert(JSON.stringify(this.form))
+    }
   }
 }
 </script>
