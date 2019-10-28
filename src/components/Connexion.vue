03ff53eb46bc356101b6d19b54016d5fede9af0b
PaulMansard <mansard.paul@gmail.com>
2019-10-28 17:04:03 +0100

    connexion obligatoire

--- src/components/Connexion.vue
+++ src/components/Connexion.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
     <h3>Connexion</h3>
-    <b-form @submit="onSubmit">
+    <b-form @submit.prevent="onSubmit()">
       <b-form-group label-cols-sm="2" label-cols-lg="2" label-for="input-horizontal" id="groupNom" label="Nom : ">
-        <b-form-input class="w-75" id="input-horizontal" placeholder="Entrez votre nom" type="text" v-model="form.nom"></b-form-input>
+        <b-form-input class="w-75" id="input-horizontal" placeholder="Entrez votre nom" type="text" v-model="form.nom" required />
       </b-form-group>
       <b-form-group id="groupPrenom" label="Prénom : " label-cols-sm="2" label-cols-lg="2" label-for="input-2">
-        <b-form-input class="w-75" id="inputPrenom" placeholder="Entez votre prénom" v-model="form.prenom"></b-form-input>
+        <b-form-input class="w-75" id="inputPrenom" placeholder="Entez votre prénom" v-model="form.prenom" required />
       </b-form-group>
       <b-form-group id="inputEntreprise" label="Entreprise : " label-cols-sm="2" label-cols-lg="2" label-for="input-2">
-        <b-form-input class="w-75" id="inputEntrerise" placeholder="Entez votre entreprise" v-model="form.entreprise"></b-form-input>
+        <b-form-input class="w-75" id="inputEntrerise" placeholder="Entez votre entreprise" v-model="form.entreprise" required />
       </b-form-group>
-      <b-button @click="onSubmit()" type="submit" variant="primary">Connexion</b-button>
+      <b-button type="submit" variant="primary">Connexion</b-button>
     </b-form>
   </div>
 
