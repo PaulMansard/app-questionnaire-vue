d10f4d14a97f5a2f85db5a61e8889d439b33f37d
PaulMansard <mansard.paul@gmail.com>
2019-11-19 16:54:22 +0100

    modification connexion

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import Questionnaire from '../views/Questionnaire.vue'
 import Accueil from '../views/Accueil.vue'
 
 Vue.use(VueRouter)
-
+//Mise en place des différente route de l'application
 const routes = [
   {
     path: '/',
